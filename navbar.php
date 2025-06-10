44de1c488c977607324d1a16b627bbdfe65bd9ca
boogiefine <se398316@ucf.edu>
2025-06-09 22:44:49 -0400

    Update navbar.php
    Quick clean up - capitalization and spelling of Nexus

--- navbar.php
+++ navbar.php
@@ -1,11 +1,13 @@
+// Changed name in line 6 to Nexus and capitalized S and I in line 11
+
 <nav>
   <ul>
     <li>
-      Nexsus
+      Nexus
     </li>
     <li><a href="index.php">Home</a></li>
     <li><a href="create.php">Add Contact</a></li>
     <li><a href="#">About</a></li>
-    <li><a href="#">sign in</a></li>
+    <li><a href="#">Sign In</a></li>
   </ul>
 </nav>
